edd9587aa3a583a97c04eae8335d576a061fa1e6
duyle <duyleekun@gmail.com>
2023-03-13 09:34:53 +0700

    chore: setup ios

--- ios/YandexLogin.swift
+++ ios/YandexLogin.swift
@@ -5,4 +5,13 @@ class YandexLogin: NSObject {
   func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
     resolve(a*b)
   }
+    
+    @objc(activate:)
+    func activate(appId: String) -> Void {
+        do {
+            try YXLSdk.shared.activate(withAppId: appId)
+        } catch {
+            // process error
+        }
+    }
 }
